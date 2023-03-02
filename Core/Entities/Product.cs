1b17ad07f1bcbb0a2034de9502d781b0bcef38b8
LNithish <nithishkumarvip@gmail.com>
2023-03-02 16:13:56 +0530

    Before New Migration

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,10 +1,18 @@
 ﻿namespace Core.Entities
 {
-    public class Product
+    public class Product: BaseEntity
     {
-        public int Id { get; set; }
+        //Below field is covered from base class
+        //public int Id { get; set; }
         //Nullale property has been disabled in Core.csproj file, no need to specify '?' or set null reference
         // public string? Summary { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string picturUrl { get; set; }
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
 }
